d015f9673af2b53552b08fa21a34458a5e8dd9c3
Ilya <ilia.petro2014@yandex.ru>
2024-11-06 18:33:16 +0400

    Added parameterized test

--- src/test/java/ru/netology/services/VacationServiceTest.java
+++ src/test/java/ru/netology/services/VacationServiceTest.java
@@ -2,34 +2,36 @@ package ru.netology.services;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 public class VacationServiceTest {
 
-    @Test
-    void calculateVacationMonthLowIncome(){
+    @ParameterizedTest
+    @CsvFileSource(files="src/test/resources/vacation.csv")
+    public void calculateVacationMonth(int income, int expenses, int threshold, int expected){
         VacationService service = new VacationService();
 
-        int income = 10_000;
-        int expenses = 3_000;
-        int threshold = 20_000;
-
-        int expected = 3;
+//        int income = 10_000;
+//        int expenses = 3_000;
+//        int threshold = 20_000;
+//        int expected = 3;
         int actual = service.vacationCalculate(income,expenses, threshold);
 
         Assertions.assertEquals(expected, actual);
     }
 
-    @Test
-    void calculateVacationMonthBigIncome(){
-        VacationService service = new VacationService();
-
-        int income = 100_000;
-        int expenses = 60_000;
-        int threshold = 150_000;
-
-        int expected = 2;
-        int actual = service.vacationCalculate(income,expenses, threshold);
-
-        Assertions.assertEquals(expected, actual);
-    }
+//    @Test
+//    void calculateVacationMonthBigIncome(){
+//        VacationService service = new VacationService();
+//
+//        int income = 100_000;
+//        int expenses = 60_000;
+//        int threshold = 150_000;
+//
+//        int expected = 2;
+//        int actual = service.vacationCalculate(income,expenses, threshold);
+//
+//        Assertions.assertEquals(expected, actual);
+//    }
 }
